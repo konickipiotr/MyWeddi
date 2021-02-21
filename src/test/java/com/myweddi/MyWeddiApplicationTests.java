6091ee204395235b6db773f7885675695c596986
Piotr <konicki.piotr@gmail.com>
2021-02-21 20:05:45 +0100

    start project

---
+++ src/test/java/com/myweddi/MyWeddiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myweddi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWeddiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
