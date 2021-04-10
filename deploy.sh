0f868c45030215d17570aad6174c9977b4506779
Piotr <konicki.piotr@gmail.com>
2021-04-10 14:01:41 +0200

    deploy v0.8.7

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,6 @@
-app_process_id=`/bin/ps -fu $USER|grep "MyWeddi-0.0.1-SNAPSHOT.jar" |awk '{print $2}'`
+app_process_id=`/bin/ps -fu $USER|grep "[M]yWeddi-0.0.1-SNAPSHOT.jar" |awk '{print $2}'`
 echo $app_process_id
-
-pwd
-ls -l
-
-ls -l target
-
 kill -9 $app_process_id
-kill -9 app_process_id
 java -jar target/MyWeddi-0.0.1-SNAPSHOT.jar &
 
 
