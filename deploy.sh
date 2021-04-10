726d724ab76712e03124bf941d263f71786ba63f
Piotr <konicki.piotr@gmail.com>
2021-04-10 12:03:40 +0200

    deploy v0.8

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 app_process_id=`/bin/ps -fu $USER|grep "MyWeddi-0.0.1-SNAPSHOT.jar" |awk '{print $2}'`
 echo $app_process_id
 
-kill -9 $$app_process_id
-java -jar MyWeddi-0.0.1-SNAPSHOT.jar &
+kill -9 $app_process_id
+java -jar target/MyWeddi-0.0.1-SNAPSHOT.jar &
+
+service jenkins restart
 
