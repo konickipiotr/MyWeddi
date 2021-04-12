bbb77b58eb1f04d3e80fd05ed3fa362d638c6012
Piotr K <33908104+konickipiotr@users.noreply.github.com>
2021-04-12 20:26:38 +0200

    Jenkins config (#16)
    * deploy v0.1
    
    * deploy v0.2
    
    * deploy v0.3
    
    * deploy v0.4
    
    * deploy v0.4.1
    
    * deploy v0.5
    
    * deploy v0.6
    
    * deploy v0.7
    
    * deploy v0.7.1
    
    * deploy v0.7.2
    
    * deploy v0.8
    
    * deploy v0.8.1
    
    * deploy v0.8.2
    
    * deploy v0.8.4
    
    * deploy v0.8.5
    
    * deploy v0.8.6
    
    * deploy v0.8.7

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+app_process_id=`/bin/ps -fu $USER|grep "[M]yWeddi-0.0.1-SNAPSHOT.jar" |awk '{print $2}'`
+echo $app_process_id
+kill -9 $app_process_id
+java -jar target/MyWeddi-0.0.1-SNAPSHOT.jar &
+
+
